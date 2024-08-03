0a20f31903ca8b0bab99f0a71447be097a8cb181
Federico Secco <federico.secco7@gmail.com>
2024-08-03 13:05:05 +0200

    Transloco

--- apps/play-clock/src/app/app.config.ts
+++ apps/play-clock/src/app/app.config.ts
@@ -1,7 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideTransloco } from '@jsverse/transloco';
+import { TranslocoHttpLoader } from './transloco/transloco.loader';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(appRoutes)],
+    providers: [
+        provideRouter(appRoutes),
+        provideHttpClient(),
+        provideTransloco({
+            config: {
+                availableLangs: ['en', 'it'],
+                defaultLang: 'en',
+                reRenderOnLangChange: true,
+                prodMode: !isDevMode(),
+            },
+            loader: TranslocoHttpLoader,
+        }),
+    ],
 };
--- apps/play-clock/src/app/pages/settings/settings.component.ts
+++ apps/play-clock/src/app/pages/settings/settings.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TimerComponent } from '@fsecco/timer';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
     selector: 'app-settings',
     standalone: true,
-    imports: [CommonModule, TimerComponent],
+    imports: [CommonModule, TranslocoModule],
     templateUrl: './settings.component.html',
     styleUrl: './settings.component.scss',
 })
