e71daf37aa5e46ce0145ee9179019b041686367e
Federico Secco <federico.secco7@gmail.com>
2024-11-19 20:26:29 +0100

    background.js in build folder

--- apps/test-kit/src/background.ts
+++ apps/test-kit/src/background.ts
@@ -1,5 +1,5 @@
-console.log('Service Worker (TS) Loaded');
-
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension Installed');
+chrome.tabs.onUpdated.addListener((tabId) => {
+    chrome.sidePanel.setOptions({ tabId, path: 'index.html', enabled: true }).catch(console.error);
 });
+
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(console.error);
