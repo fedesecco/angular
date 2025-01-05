4f1415b23f86875e018f6a08f781ad5cff702649
Federico Secco <federico.secco7@gmail.com>
2025-01-05 12:03:22 +0100

    Working animations

--- apps/test-kit/src/app/app.config.ts
+++ apps/test-kit/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideExperimentalZonelessChangeDetection(), provideRouter(appRoutes)],
+    providers: [provideExperimentalZonelessChangeDetection(), provideRouter(appRoutes), provideAnimations()],
 };
--- apps/test-kit/src/app/views/home/home.component.ts
+++ apps/test-kit/src/app/views/home/home.component.ts
@@ -1,17 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
+import { interval } from 'rxjs';
 import { SharedService } from '../../services/shared.service';
 
 @Component({
     selector: 'tk-home',
     standalone: true,
-    imports: [CommonModule, ReactiveFormsModule, FormsModule, InputTextModule, FloatLabelModule],
+    imports: [CommonModule, ReactiveFormsModule, FormsModule, InputTextModule, FloatLabelModule, ButtonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
 export class HomeComponent {
     protected sharedServ = inject(SharedService);
+    protected tot = signal(0);
+    constructor() {
+        interval(100).subscribe({
+            next: () => {
+                this.tot.set(this.tot() + 5);
+            },
+        });
+    }
 }
