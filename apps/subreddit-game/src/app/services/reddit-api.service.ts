3c95e65829b86f975d89c7b7ab3fa2380d946b7f
Federico Secco <federico.secco7@gmail.com>
2025-09-18 18:40:29 +0200

    Compile

--- apps/subreddit-game/src/app/services/reddit-api.service.ts
+++ apps/subreddit-game/src/app/services/reddit-api.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { env } from '@subredditgame/shared/constants';
 import { Observable, shareReplay } from 'rxjs';
 
 @Injectable({
@@ -11,7 +10,7 @@ export class RedditApiService {
     getAppOnlyToken(): Observable<any> {
         const body = 'grant_type=client_credentials';
         const headers = new HttpHeaders({
-            Authorization: `Basic ${btoa(`${env.REDDIT_CLIENT_ID}:${env.REDDIT_CLIENT_SECRET}`)}`,
+            Authorization: `Basic `,
             'Content-Type': 'application/x-www-form-urlencoded',
         });
         return this.http.post('https://www.reddit.com/api/v1/access_token', body, { headers }).pipe(shareReplay(1));
